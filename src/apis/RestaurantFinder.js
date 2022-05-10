881068b282ffd49996e9d77ef64b1f300bb82d3a
Sylvain.trs <sylvain.tremblais41@gmail.com>
2022-05-11 00:13:49 +0200

    update-api-url

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/api/v1/restaurants'
+    baseURL: 'https://yelp-clone-backend.herokuapp.com/api/v1/restaurants'
 })
